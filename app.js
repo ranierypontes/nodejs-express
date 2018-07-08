0794ff45f8c38ed85748dd476f51054a8c832e8d
Raniery Pontes <pontesraniery@gmail.com>
2018-07-08 01:30:48 -0300

    Servindo arquivos estáticos

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var path = require("path");
+var http = require("http");
+var app = express();
+var routes = require("./routes");
+var bodyParser = require("body-parser");
+
+app.get("/", function(req, res) {
+    res.send("Hello from ExpressJS!");
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use("/hello", routes);
+
+app.use("/public", express.static(path.join(__dirname, "public")));
+
+http.createServer(app).listen(3000, function() {
+    console.log("Express started!");    
+});
+
+// app.listen(3000, function() {
+//     console.log("Express started!");
+// });
---
+++ routes.js
@@ -0,0 +1,47 @@
+var express = require("express");
+var router = express.Router();
+
+router.get("/", function(req, res) {
+    res.json({
+        message: "Hello world"
+    });
+});
+
+router.get("/a?r", function(req, res) {
+    res.send("router a?r");
+});
+
+router.get("/b+r", function(req, res) {
+    res.send("router b+r");
+});
+
+router.get("/c*r", function(req, res) {
+    res.send("router c*r");
+});
+
+router.get("/params/:name", function(req, res) {
+    res.json({
+        params: req.params,
+        host: req.hostname,
+        headers: req.headers
+    });
+});
+
+router.post("/body", function(req, res) {
+    res.json(req.body);
+});
+
+router.get("/res", function(req, res) {
+    res.status(202).json({
+        name: 'Leonan',
+        lastname: 'Pontes'
+    });
+});
+
+// router.get("/render", function(req, res) {
+//     res.render('index', {
+
+//     });
+// });
+
+module.exports = router;
